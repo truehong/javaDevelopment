3995cd6f42a3ef52493cd8e82300b869dfd651cf
truehong <hhhhhongse@gmail.com>
2021-12-24 14:42:02 +0900

    Initial commit

---
+++ src/main/java/com/example/javadevelopment/JavaDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.javadevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javadevelopment/JavaDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javadevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
